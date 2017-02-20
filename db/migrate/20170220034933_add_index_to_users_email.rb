8ebdbeeac715ca900e623382df32bd26e356fef9
shinplus <shin.php@gmail.com>
2017-02-20 12:29:43 +0800

    Make a basic User model

---
+++ db/migrate/20170220034933_add_index_to_users_email.rb
@@ -0,0 +1,5 @@
+class AddIndexToUsersEmail < ActiveRecord::Migration[5.0]
+  def change
+  	add_index :users, :email, unique: trueP
+  end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  
+	def setup
+		@user = User.new(name: "Example User", 
+										 email: "user@example.com",
+										 password: "foobar",
+										 password_confirmation: "foobar")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
+
+	test "name should be present" do
+		@user.name = " "
+		assert_not @user.valid?
+	end
+
+	test "email should be present" do
+		@user.email = " "
+		assert_not @user.valid?
+	end
+
+	test "name should not be too long" do
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end
+
+	test "email address should be unique" do
+		dup_user = @user.dup 
+		dup_user.email = @user.email.upcase
+		@user.save
+		assert_not dup_user.valid?
+	end
+
+	test "email address should be saved as lower-case" do
+		mixed_case_email = "Foo@Example.com"
+		@user.email = mixed_case_email
+		@user.save 
+		assert_equal mixed_case_email.downcase, @user.email
+	end
+
+end
