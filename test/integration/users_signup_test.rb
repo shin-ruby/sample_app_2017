f00caf3bf6dcc615061c82ccdc1ad6f14e1ff963
shinplus <shin.php@gmail.com>
2017-03-01 15:13:09 +0800

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup info" do
   	get signup_path
   	assert_no_difference 'User.count' do
@@ -25,6 +29,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       } 
     }
     end
+
+    assert_equal 1, ActionMailer::Base.deliveries.size 
+    user = assigns(:user)
+    assert_not user.activated?
+
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    get edit_account_activation_path("invalid token", email: user.email) 
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: 'wrong') 
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: user.email) 
+    assert user.reload.activated?
+
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
